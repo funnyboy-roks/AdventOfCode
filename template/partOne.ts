53a3f04d45553e07dfe7ce82fcba876667072c55
funnyboy-roks <funnyboyroks@gmail.com>
2021-10-15 11:38:36 -0500

    Update template

--- template/partOne.ts
+++ template/partOne.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 
-const input = fs.readFileSync('Day5/input.txt', 'utf8');
+const day = 5;
+
+const input = fs.readFileSync(`Day${day}/input.txt`, 'utf8');
 
 const parseData = (data: string) => {
 
