bd8b8fed29ab76f21f37ca687877d8ee735bc729
funnyboy-roks <funnyboyroks@gmail.com>
2021-10-15 11:37:51 -0500

    Create Template

---
+++ template/partOne.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('Day5/input.txt', 'utf8');
+
+const parseData = (data: string) => {
+
+}
+
+const main = (): void => {
+
+    const data = parseData(input);
+
+    
+
+}
+
+main();
