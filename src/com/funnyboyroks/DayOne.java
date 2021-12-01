16a50ba6ecb84115be1eadd07a7d730857123597
funnyboy-roks <funnyboyroks@gmail.com>
2021-12-01 10:07:51 -0600

    Day1

---
+++ src/com/funnyboyroks/DayOne.java
@@ -0,0 +1,49 @@
+package com.funnyboyroks;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class DayOne {
+
+    public static void main(String[] args) {
+        String input = Util.getInput("input1.txt");
+        String[] data = parseData(input.trim());
+
+//        partOne(data); // ##:##
+        partTwo(data); // ##:##
+    }
+
+    public static String[] parseData(String input) {
+        return input.split("");
+    }
+
+    public static void partOne(String[] data) {
+        String prev = data[0];
+        int sum = 0;
+        for (int i = 1; i <= data.length; i++) {
+            int p = i % data.length;
+            String s = data[p];
+            if(prev.equals(s)) {
+                sum += Integer.parseInt(s);
+            }
+            prev = s;
+        }
+
+        System.out.println(sum);
+    }
+
+    public static void partTwo(String[] data) {
+        int sum = 0;
+        for (int i = 0; i < data.length; i++) {
+            String s = data[i];
+            String next = data[(i + data.length/2) % data.length];
+            if(s.equals(next)) {
+                sum += Integer.parseInt(s);
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
