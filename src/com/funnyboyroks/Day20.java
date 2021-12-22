436c5648e1a8d9e77bff89df7130e76e066d4e18
funnyboy-roks <funnyboyroks@gmail.com>
2021-12-21 21:06:19 -0600

    Day 20 Optimisation

--- src/com/funnyboyroks/Day20.java
+++ src/com/funnyboyroks/Day20.java
@@ -14,14 +14,14 @@ public class Day20 {
     public static void main(String[] args) {
         List<String> input = Util.lines(20);
 
-//        input = """
-//            ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
-//
-//            #..#.
-//            #....
-//            ##..#
-//            ..#..
-//            ..###""".lines().map(String::trim).toList();
+        input = """
+            ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+            #..#.
+            #....
+            ##..#
+            ..#..
+            ..###""".lines().map(String::trim).toList();
 
         Tuple.Two<String, List<String>> data = parseData(input);
 
@@ -32,8 +32,8 @@ public class Day20 {
     }
 
     private static Tuple.Two<String, List<String>> parseData(List<String> input) {
-        String algo = input.get(0);
-        List<String> image = input.subList(2, input.size());
+        String algo = input.get(0).replaceAll("#", "1").replaceAll("\\.", "0");
+        List<String> image = input.subList(2, input.size()).stream().map(s -> s.replaceAll("#", "1").replaceAll("\\.", "0")).toList();
         return new Tuple.Two<>(algo, new ArrayList<>(image));
     }
 
@@ -48,62 +48,49 @@ public class Day20 {
     }
 
     public static int run(String algo, List<String> image, int cycles) {
-        for (int loop = 0; loop < cycles * 3; loop++) {
-            expand(image);
-        }
-//        System.out.println(image.stream().map(Object::toString).collect(Collectors.joining("\n")));
+        print(image);
 
         for (int loop = 0; loop < cycles; loop++) {
-//            expand(image);
-//            print(image);
             List<String> newImage = new ArrayList<>();
-            for (int y = 1; y < image.size() - 1; y++) {
+            expand(image, 2);
+            for (int y = 0; y < image.size(); y++) {
                 String str = "";
-                for (int x = 1; x < image.get(y).length() - 1; x++) {
+                for (int x = 0; x < image.get(y).length(); x++) {
                     String current = "";
-                    for (int i = -1; i <= 1; i++) {
-                        for (int j = -1; j <= 1; j++) {
-                            if (x + i < 0 || y + j < 0 || x + i >= image.get(y + j).length() || y + j >= image.size()) {
-                                current += getInsideBorder(image);
-                                continue;
-                            }
-                            current += image.get(y + i).charAt(x + j);
-                        }
+                    if (y > 0) {
+                        current += image.get(y - 1).substring(Math.max(0, x - 1), Math.min(x + 2, image.get(y).length()));
                     }
-//                    current += image.get(Math.max(0, y - 1)).substring(Math.max(0, x - 1), Math.min(x + 2, image.get(y).length()));
-//                    current += image.get(y).substring(Math.max(0, x - 1), Math.min(x + 2, image.get(y).length()));
-//                    current += image.get(Math.min(y + 1, image.size() - 1)).substring(Math.max(0, x - 1), Math.min(x + 2, image.get(y).length()));
-                    int n = Integer.parseInt(current.replaceAll("#", "1").replaceAll("\\.", "0"), 2);
+                    current += image.get(y).substring(Math.max(0, x - 1), Math.min(x + 2, image.get(y).length()));
+                    if (y < image.size() - 1) {
+                        current += image.get(Math.min(y + 1, image.size() - 1)).substring(Math.max(0, x - 1), Math.min(x + 2, image.get(y).length()));
+                    }
+                    int n = Integer.parseInt(current, 2);
                     str += algo.charAt(n);
                 }
                 newImage.add(str);
             }
             image = newImage;
+            shrink(image);
         }
-
         System.out.println();
 
-        shrink(image);
-        shrink(image);
-        shrink(image);
-        shrink(image);
-        shrink(image);
-        shrink(image);
-        shrink(image);
-        shrink(image);
         print(image);
 
         return sum(image);
     }
 
-    public static void expand(List<String> list) {
+    public static void expand(List<String> list, int amount) {
+        String c = "0".repeat(amount);
         for (int i = 0; i < list.size(); i++) {
             String l = list.get(i);
-            list.set(i, (".%s.").formatted(l));
+            list.set(i, c + l + c);
         }
-        String str = list.get(0).replaceAll(".", ".");
-        list.add(0, str);
-        list.add(str);
+        String str = list.get(0).replaceAll(".", "0");
+        for (int i = 0; i < amount; i++) {
+
+            list.add(0, str);
+            list.add(str);
+        }
     }
 
     public static void shrink(List<String> list) {
@@ -116,26 +103,14 @@ public class Day20 {
         list.addAll(newList);
     }
 
-//    public static void print(List<List<String>> l) {
-//        for (List<String> row : l) {
-//            System.out.println(String.join("", row));
-//        }
-//    }
-
-    public static char getInsideBorder(List<String> grid) {
-        String top = grid.get(1).substring(1, grid.get(1).length() - 1);
-        String bottom = grid.get(grid.size() - 2).substring(1, grid.get(1).length() - 1);
-        return top.equals(bottom) ? top.charAt(0) : '.';
-    }
-
     public static void print(List<String> l) {
-        System.out.println(String.join("\n", l));//.replaceAll("#", "█").replaceAll("\\.", " "));
+        System.out.println(String.join("\n", l).replaceAll("1", "#").replaceAll("0", "."));
     }
 
     public static int sum(List<String> l) {
         int sum = 0;
         for (String row : l) {
-            sum += row.chars().filter(c -> c == '#').count();
+            sum += row.chars().filter(c -> c == '1').count();
         }
         return sum;
     }
